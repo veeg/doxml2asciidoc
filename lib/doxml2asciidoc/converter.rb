550bbc606c67b0206a1f393e2d55980a1d032977
Vegard Sandengen <vegardsandengen@gmail.com>
2016-05-06 20:36:58 +0200

    Add enum and primitive typedef support
    Improve output

--- lib/doxml2asciidoc/converter.rb
+++ lib/doxml2asciidoc/converter.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 module Doxml2AsciiDoc
 class Converter
 
-  @@verbose = false
+  @@verbose = true
 
   def initialize opts = {}
     @root = nil
@@ -21,15 +21,29 @@ class Converter
 
   def verbose msg
     if @@verbose
-      puts msg 
+      puts msg
     end
   end
 
   def convert
-    hsh =  nil
-
     verbose "Converting"
 
+    hsh = parse @root
+    ad = AsciidocOutput.new hsh
+    ad.generate
+  end
+
+  def self.parse_file infile
+    str = ::IO.read infile
+    xmldoc = ::Nokogiri::XML::Document.parse str
+    converter = Converter.new xmldoc
+    converter.verbose "Parsing input file: #{infile}"
+    converter.parse(xmldoc.root)
+  end
+
+  def parse root
+    @root = root
+    hsh =  nil
     if @root.name.eql? "doxygenindex"
       verbose "Input root is DoxygenIndex"
       hsh = parse_doxygenindex @root
@@ -46,11 +60,16 @@ class Converter
       raise "Unhandled/Unknown root element: " + @root.name
     end
 
-    generate_asciidoc hsh
+    hsh
   end
 
   # Assume the input doc rootelement is 'doxygenindex'
-  def parse_doxygenindex root 
+  def parse_doxygenindex root
+
+    hsh = {:name => "Index",
+           :files => [],
+    }
+
     root.xpath('//compound').each do |compound|
 
       xmldir = ::FileUtils.pwd
@@ -61,7 +80,8 @@ class Converter
 
       case compound['kind']
       when 'file'
-        Doxml2AsciiDoc.convert_file filepath
+        h = Converter.parse_file filepath
+        hsh[:files] << h
       when 'struct'
       when 'dir'
         # Silently ignore this compound - we dont care about it.
@@ -69,15 +89,19 @@ class Converter
         STDERR.puts "Unhandled doxygenindex compound kind: " + compound['kind']
       end
     end
+
+    hsh
   end
 
-  def parse_doxygenfile root 
+  def parse_doxygenfile root
     compound = root.at_xpath '//compounddef'
 
     hsh = {:name => compound.element_children.at_xpath('//compoundname').text,
            :id => compound['id'],
            :language => compound['language'],
            :functions => [],
+           :enums => [],
+           :typedefs => []
           }
 
     compound.xpath('./sectiondef').each do |section|
@@ -88,9 +112,12 @@ class Converter
       when 'func'
        ret = parse_sectiondef_func section
        hsh[:functions].concat ret[:functions]
-
       when 'typedef'
-        parse_sectiondef_typedef section
+        ret = parse_sectiondef_typedef(section)
+        hsh[:typedefs].concat(ret)
+      when 'enum'
+        ret = parse_sectiondef_enum(section)
+        hsh[:enums].concat(ret)
       else
         raise "Unhandled section kind " + section['kind']
       end
@@ -100,6 +127,62 @@ class Converter
   end
 
   def parse_sectiondef_define section
+  end
+
+  def parse_sectiondef_typedef section
+    typedefs = []
+
+    section.xpath('./memberdef').each do |member|
+      case member['kind']
+      when 'typedef'
+        hsh = {}
+        hsh[:name] = member.at_xpath('./name').text
+        hsh[:type] = member.at_xpath('./type').text
+        detail = member.at_xpath('./detaileddescription')
+        if detail
+          hsh[:doc] = detail.text
+        end
+        typedefs << hsh
+      else
+        raise "member kind not typedef in sectiondef typedef: #{meber['kind']}"
+      end
+    end
+
+    typedefs
+  end
+
+  def parse_sectiondef_enum section
+    enums = []
+    section.xpath('./memberdef').each do |member|
+      case member['kind']
+      when 'enum'
+        hsh = {}
+        hsh[:name] = member.at_xpath('./name').text
+
+        brief = member.at_xpath('./briefdescription/para')
+        if brief
+          hsh[:doc] = brief.text
+        end
+
+        hsh[:enums] = []
+        member.xpath('./enumvalue').each do |enum|
+          e = {}
+          e[:name] = enum.at_xpath('./name').text
+          # Get the first para in briefdescription
+          brief = enum.at_xpath('./briefdescription/para')
+          if brief
+            # brief is contained in one or more para children?
+            e[:doc] = brief.text
+          end
+          hsh[:enums] << e
+        end
+        enums << hsh
+      else
+        raise "member kind not enum in sectiondef enum: (#{member['kind']}"
+      end
+    end
+
+    enums
   end
 
   def parse_sectiondef_func section
@@ -115,12 +198,12 @@ class Converter
 
         params = []
         member.xpath('./param').each do |param|
-          param = {:type => param.at_xpath('./type').text, 
+          param = {:type => param.at_xpath('./type').text,
                    :declname => param.at_xpath('./declname').text}
           params.push param
         end
         hsh[:params] = params
-        hsh[:return] = ""
+        hsh[:return] = []
         detail = member.at_xpath('./detaileddescription')
         if detail
           hsh[:detail] = []
@@ -149,8 +232,18 @@ class Converter
                     end
                     codeblock += line
                   end
-                  hsh[:detail].push :type => :code, :value => codeblock 
-                                  
+                  hsh[:detail].push :type => :code, :value => codeblock
+
+                elsif child.element? and child.name.eql? "itemizedlist"
+                  listitems = child.xpath('./listitem')
+                  next if listitems.nil?
+
+                  list = []
+                  listitems.each do |item|
+                    list << item.at_xpath('./para').text
+                  end
+                  hsh[:detail].push :type => :list, :value => list
+
                 elsif child.element? and child.name.eql? "parameterlist"
                   # Parameters
                   parameters = child.xpath('./parameteritem')
@@ -174,13 +267,13 @@ class Converter
                 elsif child.element? and child.name.eql? 'simplesect'
                   case child['kind']
                   when "return"
-                    hsh[:return] =  child.text
+                    hsh[:return] <<  child.text
                   else
                     STDERR.puts 'detailed description -> simplesect kind not handled: ' + child['kind']
                   end
-                  
+
                 else
-                  STDERR.puts "detailed description paramter child not handlled: " + child.name
+                  STDERR.puts "detailed description parameter child not handled: " + child.name
                 end
               end
 
@@ -189,7 +282,7 @@ class Converter
         end
 
         hsh[:brief] = member.at_xpath('./briefdescription').text
-      
+
         functions.push hsh
       else
         raise "Unhandled sectiondef->memberdef kind " + member['kind']
@@ -214,78 +307,165 @@ class Converter
     line
   end
 
+end
 
-  def parse_sectiondef_typedef section
-  end
+  class AsciidocOutput
+    def initialize hsh = {}
+      @str = ""
+      @files = []
+      @name = hsh[:name]
 
-
-  def generate_asciidoc hsh
-    str = "= #{hsh[:name]} API Documentation\n"
-    str += ":source-highlighter: coderay\n"
-    str += "\n"
-
-    str += "== Functions\n"
-    str += "\n"
-    
-    hsh[:functions].each do |func|
-
-      str += "== #{func[:function_name]}\n"
-      str += "\n"
-      str += "[cols='h,5a']\n"
-      str += "|===\n"
-      str += "| Description\n"
-      str += "| #{func[:brief]}\n"
-      str += "\n"
-
-      str += "| Signature \n"
-      str += "|\n"
-      str += "[source,C]\n"
-      str += "----\n"
-      str += "#{func[:definition]} #{func[:argsstring]}\n" 
-      str += "----\n"
-      str += "\n"
-
-      str += "| Parameters\n"
-      str += "|\n"
-      func[:params].each do |param|
-        str += "#{parameter_direction_string param}`#{param[:type]} #{param[:declname]}`::\n"
-        str += "#{param[:description]}\n"
+      if hsh.has_key?(:files)
+        @files = hsh[:files]
+      else
+        @files << hsh
       end
-      str += "\n"
+    end
 
-      str += "| Return\n"
-      str += "| #{func[:return]} \n"
-      str += "\n"
+    def generate
+
+      puts "Generating outputfile for: #{@name}"
+
+      @str = "= #{@name} API Documentation\n"
+      @str += ":source-highlighter: coderay\n"
+      @str += ":toc: left\n"
+      @str += "\n"
+
+      #output_typedefs
+
+
+      output_enums
+
+      @str += "== Functions\n"
+      @str += "\n"
+
+      @files.each do |hsh|
+        hsh[:functions].each do |func|
+          single_function func
+        end
+      end
+      @str += "\n"
+    end
+
+    def output_typedefs
+      typedefs = []
+      @files.each do |hsh|
+        if hsh.has_key? :typedefs
+          hsh[:typedefs].each do |typedef|
+            typedefs << typedef
+          end
+        end
+      end
+      if typedefs.length > 0
+        @str += "== Typedefs\n"
+        @str += "\n"
+        typedefs.each do |typedef|
+          single_typedef typedef
+        end
+        @str += "\n"
+      end
+    end
+
+    def output_enums
+      @str += "== Enums\n"
+      @str += "\n"
+      @files.each do |hsh|
+        hsh[:enums].each do |enum|
+          single_enum enum
+        end
+      end
+      @str += "\n"
+    end
+
+    def single_typedef typedef
+      @str += "=== #{typedef[:name]}\n"
+      @str += "\n"
+      @str += "[horizontal]\n"
+      @str += "#{typedef[:type]} -> #{typedef[:name]}:: #{typedef[:doc]}\n"
+    end
+
+    def single_enum enum
+      @str += "=== #{enum[:name]}\n"
+      @str += "\n"
+      @str += enum[:doc] if enum[:doc]
+      @str += "\n"
+      @str += "[horizontal]\n"
+      enum[:enums].each do |e|
+        doc = e[:doc]
+        doc ||= "No documentation entry."
+        @str += "#{e[:name]}:: #{doc}\n"
+      end
+      @str += "\n"
+    end
+
+    def single_function func
+
+      @str += "=== #{func[:function_name]}\n"
+      @str += "\n"
+      @str += "[cols='h,5a']\n"
+      @str += "|===\n"
+      @str += "| Description\n"
+      @str += "| #{func[:brief]}\n"
+      @str += "\n"
+
+      @str += "| Signature \n"
+      @str += "|\n"
+      @str += "[source,C]\n"
+      @str += "----\n"
+      @str += "#{func[:definition]} #{func[:argsstring]}\n"
+      @str += "----\n"
+      @str += "\n"
+
+      @str += "| Parameters\n"
+      @str += "|\n"
+      func[:params].each do |param|
+        @str += "#{parameter_direction_string param}`#{param[:type]} #{param[:declname]}`::\n"
+        @str += "#{param[:description]}\n"
+      end
+      @str += "\n"
+
+      if func[:return].length > 0
+        @str += "| Return\n"
+        @str += "| "
+        func[:return].each do |ret|
+          @str += "* #{ret} \n"
+        end
+        @str += "\n"
+      end
 
       # All entries have one '\n' :text entry - Ignore this section if so.
       if func[:detail].length > 2
-        str += "| Details / Examples \n"
-        str += "|\n"
+        @str += "| Details / Examples \n"
+        @str += "|\n"
         func[:detail].each do |detail|
           if detail[:type] == :code
-          str += "----\n"
-          str += "#{detail[:value]}\n"
-          str += "----\n"
+            @str += "----\n"
+            @str += "#{detail[:value]}\n"
+            @str += "----\n"
           elsif detail[:type] == :text
-            str += "#{detail[:value]}\n"
+            @str += "#{detail[:value]}\n"
+          elsif detail[:type] == :list
+            @str += "\n"
+            detail[:value].each do |txt|
+              @str += " * #{txt}\n"
+            end
+            @str += "\n\n"
           end
         end
-        str += "\n"
+        @str += "\n"
       end
-      str += "|===\n"
-      str += "\n"
+
+      @str += "|===\n"
+      @str += "\n"
     end
 
-    str
-  end
-
-  def parameter_direction_string param
-    if param[:direction]
-      "*#{param[:direction]}* "
-    else
-      ""
+    def parameter_direction_string param
+      if param[:direction]
+        "*#{param[:direction]}* "
+      else
+        ""
+      end
     end
-  end
+  end # AsciidocOutput
 
-end
 end
